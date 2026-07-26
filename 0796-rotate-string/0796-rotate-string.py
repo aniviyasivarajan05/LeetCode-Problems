42793ad84cb8e5e0b90beacdb0da4a5935f28460
Aniviya Sivarajan <aniviyaksivarajan2005@gmail.com>
2026-07-26 13:43:17 +0530

    Time: 0 ms (100%), Space: 19.3 MB (51.61%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.py
+++ 0796-rotate-string/0796-rotate-string.py
@@ -1,52 +1,9 @@
 class Solution:
-    def computeLPSArray(self, pat):
-        n = len(pat)
-        lps = [0] * n
-
-        patLen = 0
-        i = 1
-
-        while i < n:
-            if pat[i] == pat[patLen]:
-                patLen += 1
-                lps[i] = patLen
-                i += 1
-            else:
-                if patLen != 0:
-                    patLen = lps[patLen - 1]
-                else:
-                    lps[i] = 0
-                    i += 1
-
-        return lps
-
-    def rotateString(self, s, goal):
+    def rotateString(self, s: str, goal: str) -> bool:
         if len(s) != len(goal):
             return False
 
-        txt = s + s
-        pat = goal
+        s = s + s
 
-        n = len(txt)
-        m = len(pat)
-
-        lps = self.computeLPSArray(pat)
-
-        i = 0
-        j = 0
-
-        while i < n:
-            if txt[i] == pat[j]:
-                i += 1
-                j += 1
-
-                if j == m:
-                    return True
-
-            else:
-                if j != 0:
-                    j = lps[j - 1]
-                else:
-                    i += 1
-
-        return False
+        # Find goal in the concatenated string
+        return goal in s
