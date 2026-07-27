82cc97bae6d73971cf823ce2027a92767b71da5d
Aniviya Sivarajan <aniviyaksivarajan2005@gmail.com>
2026-07-27 18:27:48 +0530

    Time: 285 ms (30.61%), Space: 68.4 MB (94.66%) - LeetHub

--- 0595-big-countries/0595-big-countries.py
+++ 0595-big-countries/0595-big-countries.py
@@ -1,7 +1,5 @@
 import pandas as pd
 
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
-    return world.loc[
-        (world.area >= 3000000) | (world.population >= 25000000),
-        ["name", "population", "area"]
-    ]
+    mask = (world["area"] >= 3000000) | (world["population"] >= 25000000)
+    return world.loc[mask, ["name", "population", "area"]]
