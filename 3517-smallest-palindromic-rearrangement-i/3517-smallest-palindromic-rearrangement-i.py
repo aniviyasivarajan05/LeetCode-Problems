c1747fa2e1cf3503567b0e38425e615e96c085b3
Aniviya Sivarajan <aniviyaksivarajan2005@gmail.com>
2026-07-28 19:22:15 +0530

    Time: 310 ms (40.09%), Space: 20.5 MB (96.31%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.py
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.py
@@ -3,15 +3,15 @@ class Solution:
         freq = [0] * 26
 
         for ch in s:
-            freq[ord(ch) - 97] += 1
+            freq[ord(ch) - ord('a')] += 1
 
         left = []
         mid = ""
 
         for i in range(26):
-            if freq[i] % 2:
-                mid = chr(i + 97)
-            left.append(chr(i + 97) * (freq[i] // 2))
+            if freq[i] & 1:
+                mid = chr(i + ord('a'))
+            left.append(chr(i + ord('a')) * (freq[i] // 2))
 
         left = "".join(left)
         return left + mid + left[::-1]
