f7647cb7b2e9f7654ca9952e55989b63dc9eb35e
Aniviya Sivarajan <aniviyaksivarajan2005@gmail.com>
2026-08-08 11:15:49 +0530

    Time: 415 ms (84.78%), Space: 47 MB (76.09%) - LeetHub

--- 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.py
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.py
@@ -1,31 +1,25 @@
 class Solution:
-    def validSequence(self, word1: str, word2: str) -> list[int]:
+    def validSequence(self, word1: str, word2: str) -> List[int]:
         n, m = len(word1), len(word2)
-        
-        # suff[i] stores the max length of word2's suffix matching word1[i:]
-        suff = [0] * (n + 1)
+
+        last = [-1] * m
         j = m - 1
+
         for i in range(n - 1, -1, -1):
             if j >= 0 and word1[i] == word2[j]:
+                last[j] = i
                 j -= 1
-            suff[i] = m - 1 - j
 
-        ans = []
-        j = 0
-        mismatch_used = False
-        
-        for i in range(n):
+        res = []
+        skip = j = 0
+
+        for i, c in enumerate(word1):
             if j == m:
                 break
-                
-            if word1[i] == word2[j]:
-                ans.append(i)
+
+            if c == word2[j] or skip == 0 and (j == m - 1 or i < last[j + 1]):
+                skip += c != word2[j]
+                res.append(i)
                 j += 1
-            else:
-                # Try using the single allowed mismatch here
-                if not mismatch_used and suff[i + 1] >= m - 1 - j:
-                    ans.append(i)
-                    j += 1
-                    mismatch_used = True
-                    
-        return ans if len(ans) == m else []
+
+        return res if j == m else []
