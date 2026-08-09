f406527a8dfb868b251a84b8a02acd892fec5975
Aniviya Sivarajan <aniviyaksivarajan2005@gmail.com>
2026-08-09 21:54:23 +0530

    Time: 172 ms (54.97%), Space: 19.4 MB (97.49%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.py
+++ 1140-stone-game-ii/1140-stone-game-ii.py
@@ -2,39 +2,35 @@ class Solution:
     def stoneGameII(self, piles: list[int]) -> int:
         n = len(piles)
 
-        # suffix[i] = total stones from pile i to the end
+        # suffix[i] = sum of piles[i:]
         suffix = [0] * (n + 1)
 
         for i in range(n - 1, -1, -1):
             suffix[i] = suffix[i + 1] + piles[i]
 
-        # dp[i][M] = maximum stones the current player
-        # can collect starting from index i with current M
+        # dp(i, M) = maximum stones current player can get
+        # from index i when M is M
         dp = [[0] * (n + 1) for _ in range(n + 1)]
 
-        # Process from the end towards the beginning
         for i in range(n - 1, -1, -1):
             for M in range(1, n + 1):
 
-                # If we can take all remaining piles
+                # Can take all remaining piles
                 if i + 2 * M >= n:
                     dp[i][M] = suffix[i]
                     continue
 
-                # Try taking X piles
-                best = 0
-
+                # Current player chooses X
                 for X in range(1, 2 * M + 1):
-                    # Stones taken now
-                    taken = suffix[i] - suffix[i + X]
+                    next_M = max(M, X)
 
-                    # Opponent gets the best possible result
-                    opponent = dp[i + X][max(M, X)]
+                    # After taking X piles, opponent starts at i+X
+                    opponent = dp[i + X][next_M]
 
-                    # Current player wants to maximize their stones
-                    best = max(best, taken + suffix[i + X] - opponent)
-
-                dp[i][M] = best
+                    # Total remaining stones - opponent's stones
+                    dp[i][M] = max(
+                        dp[i][M],
+                        suffix[i] - opponent
+                    )
 
         return dp[0][1]
-        
