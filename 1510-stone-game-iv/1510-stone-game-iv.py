fecb6e52d9830d908b9041f5fcf637670db31404
Aniviya Sivarajan <aniviyaksivarajan2005@gmail.com>
2026-08-10 08:51:55 +0530

    Time: 591 ms (60.61%), Space: 20.1 MB (67.88%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.py
+++ 1510-stone-game-iv/1510-stone-game-iv.py
@@ -1,15 +1,13 @@
 class Solution:
     def winnerSquareGame(self, n: int) -> bool:
         dp = [False] * (n + 1)
-
+        
         for i in range(1, n + 1):
-            j = 1
-
-            while j * j <= i:
-                if dp[i - j * j] == False:
+            k = 1
+            while k * k <= i:
+                if not dp[i - k * k]:
                     dp[i] = True
                     break
-
-                j += 1
-
+                k += 1
+                
         return dp[n]
