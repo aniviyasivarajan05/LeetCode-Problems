9fecaf1bd351e9e6399e09a978112d368ea69815
Aniviya Sivarajan <aniviyaksivarajan2005@gmail.com>
2026-08-13 05:42:21 +0530

    Time: 2453 ms (75.41%), Space: 138.5 MB (11.48%) - LeetHub

--- 2213-longest-substring-of-one-repeating-character/2213-longest-substring-of-one-repeating-character.py
+++ 2213-longest-substring-of-one-repeating-character/2213-longest-substring-of-one-repeating-character.py
@@ -1,69 +1,69 @@
-class SegmentTree:
-    def __init__(self, s: str):
-        self.n = len(s)
-        # Each node stores: (max_len, pref_len, suff_len, pref_char, suff_char, seg_len)
-        self.tree = [None] * (4 * self.n)
-        self._build(s, 1, 0, self.n - 1)
-
-    def _merge(self, left, right):
-        l_max, l_pref, l_suff, l_pchar, l_schar, l_len = left
-        r_max, r_pref, r_suff, r_pchar, r_schar, r_len = right
-
-        seg_len = l_len + r_len
-        
-        # Calculate prefix length
-        pref_char = l_pchar
-        pref_len = l_pref + (r_pref if l_pref == l_len and l_pchar == r_pchar else 0)
-
-        # Calculate suffix length
-        suff_char = r_schar
-        suff_len = r_suff + (l_suff if r_suff == r_len and r_schar == l_schar else 0)
-
-        # Calculate maximum contiguous block length
-        max_len = max(l_max, r_max)
-        if l_schar == r_pchar:
-            max_len = max(max_len, l_suff + r_pref)
-
-        return (max_len, pref_len, suff_len, pref_char, suff_char, seg_len)
-
-    def _build(self, s: str, node: int, start: int, end: int):
-        if start == end:
-            ch = s[start]
-            self.tree[node] = (1, 1, 1, ch, ch, 1)
-            return
-        
-        mid = (start + end) // 2
-        self._build(s, 2 * node, start, mid)
-        self._build(s, 2 * node + 1, mid + 1, end)
-        self.tree[node] = self._merge(self.tree[2 * node], self.tree[2 * node + 1])
-
-    def update(self, idx: int, ch: str):
-        self._update(1, 0, self.n - 1, idx, ch)
-
-    def _update(self, node: int, start: int, end: int, idx: int, ch: str):
-        if start == end:
-            self.tree[node] = (1, 1, 1, ch, ch, 1)
-            return
-        
-        mid = (start + end) // 2
-        if idx <= mid:
-            self._update(2 * node, start, mid, idx, ch)
-        else:
-            self._update(2 * node + 1, mid + 1, end, idx, ch)
-            
-        self.tree[node] = self._merge(self.tree[2 * node], self.tree[2 * node + 1])
-
-    def get_max_len(self) -> int:
-        return self.tree[1][0]
-
-
 class Solution:
-    def longestRepeating(self, s: str, queryCharacters: str, queryIndices: list[int]) -> list[int]:
-        st = SegmentTree(s)
+    def longestRepeating(self, s, queryCharacters, queryIndices):
+        n = len(s)
+        tree = [None] * (4 * n)
+
+        # (left_char, right_char, prefix, suffix, best, length)
+        def merge(a, b):
+            if a is None:
+                return b
+            if b is None:
+                return a
+
+            lc1, rc1, pre1, suf1, best1, len1 = a
+            lc2, rc2, pre2, suf2, best2, len2 = b
+
+            # Prefix
+            pre = pre1
+            if pre1 == len1 and lc1 == lc2:
+                pre = len1 + pre2
+
+            # Suffix
+            suf = suf2
+            if suf2 == len2 and rc1 == rc2:
+                suf = suf1 + len2
+
+            # Best answer
+            best = max(best1, best2)
+
+            if rc1 == lc2:
+                best = max(best, suf1 + pre2)
+
+            return (lc1, rc2, pre, suf, best, len1 + len2)
+
+        def build(node, l, r):
+            if l == r:
+                c = s[l]
+                tree[node] = (c, c, 1, 1, 1, 1)
+                return
+
+            mid = (l + r) // 2
+
+            build(node * 2, l, mid)
+            build(node * 2 + 1, mid + 1, r)
+
+            tree[node] = merge(tree[node * 2], tree[node * 2 + 1])
+
+        def update(node, l, r, idx, c):
+            if l == r:
+                tree[node] = (c, c, 1, 1, 1, 1)
+                return
+
+            mid = (l + r) // 2
+
+            if idx <= mid:
+                update(node * 2, l, mid, idx, c)
+            else:
+                update(node * 2 + 1, mid + 1, r, idx, c)
+
+            tree[node] = merge(tree[node * 2], tree[node * 2 + 1])
+
+        build(1, 0, n - 1)
+
         ans = []
-        
-        for ch, idx in zip(queryCharacters, queryIndices):
-            st.update(idx, ch)
-            ans.append(st.get_max_len())
-            
+
+        for c, idx in zip(queryCharacters, queryIndices):
+            update(1, 0, n - 1, idx, c)
+            ans.append(tree[1][4])
+
         return ans
