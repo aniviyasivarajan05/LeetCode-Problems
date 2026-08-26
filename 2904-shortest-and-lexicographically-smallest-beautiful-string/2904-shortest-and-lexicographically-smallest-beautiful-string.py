ee8ed511c89e17645b6f9a9ac0148bb57f8df72c
Aniviya Sivarajan <aniviyaksivarajan2005@gmail.com>
2026-08-26 20:28:21 +0530

    Time: 0 ms (100%), Space: 19.2 MB (72.07%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.py
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.py
@@ -1,32 +1,31 @@
 class Solution:
     def shortestBeautifulSubstring(self, s: str, k: int) -> str:
-        left = 0
-        ones = 0
+        ones = []
+
+        # Store positions of all 1s
+        for i in range(len(s)):
+            if s[i] == '1':
+                ones.append(i)
+
+        if len(ones) < k:
+            return ""
+
         best = ""
 
-        for right in range(len(s)):
-            if s[right] == '1':
-                ones += 1
+        # Consider every group of k consecutive 1s
+        for i in range(len(ones) - k + 1):
+            first = ones[i]
+            last = ones[i + k - 1]
 
-            # If we have more than k ones, move left
-            while ones > k:
-                if s[left] == '1':
-                    ones -= 1
-                left += 1
+            # Minimum substring containing these k ones
+            current = s[first:last + 1]
 
-            # We have exactly k ones
-            if ones == k:
-                # Remove leading zeros to make the substring as short as possible
-                while left <= right and s[left] == '0':
-                    left += 1
-
-                current = s[left:right + 1]
-
-                # Update if shorter or lexicographically smaller
-                if (best == "" or
-                    len(current) < len(best) or
-                    (len(current) == len(best) and current < best)):
-                    best = current
+            if best == "":
+                best = current
+            elif len(current) < len(best):
+                best = current
+            elif len(current) == len(best) and current < best:
+                best = current
 
         return best
         
