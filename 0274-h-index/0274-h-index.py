a9d7c4170bd96a68efd9e659e151700863e18053
Aniviya Sivarajan <aniviyaksivarajan2005@gmail.com>
2026-09-04 10:46:49 +0530

    Time: 0 ms (100%), Space: 19.5 MB (25.9%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -1,14 +1,25 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        
-        # Sort the citations in descending order
-        citations.sort(reverse=True)
-        
         n = len(citations)
-        idx = 0
-        
-        # Keep incrementing idx till citations[idx] > idx
-        while idx < n and citations[idx] > idx:
-            idx += 1
-        
+        freq = [0] * (n + 1)
+
+        # Count the frequency of citations
+        for citation in citations:
+            if citation >= n:
+                freq[n] += 1
+            else:
+                freq[citation] += 1
+
+        idx = n
+
+        # Variable to keep track of the count of papers
+        # having at least idx citations
+        s = freq[n]
+
+        while s < idx:
+            idx -= 1
+            s += freq[idx]
+
+        # Return the largest index for which the count of
+        # papers with at least idx citations becomes >= idx
         return idx
